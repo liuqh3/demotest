72618d5277f10196354eec3e13bad8a8df1316a6
liuqh <644911337@qq.com>
2019-06-01 11:38:48 +0800

    0.1版本

--- src/main/java/com/liu/mybatis/demotest/DemotestApplication.java
+++ src/main/java/com/liu/mybatis/demotest/DemotestApplication.java
@@ -3,6 +3,7 @@ package com.liu.mybatis.demotest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//0.1版本
 @SpringBootApplication
 public class DemotestApplication {
 
